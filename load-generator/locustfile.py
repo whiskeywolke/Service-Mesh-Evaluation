9118e73c87e264126822628704c16a431069a849
Michael Mente <mentemichael@live.com>
2022-05-09 17:10:45 +0200

    add loadgen

--- load-generator/locustfile.py
+++ load-generator/locustfile.py
@@ -1,15 +1,23 @@
+# inspired by https://github.com/GoogleCloudPlatform/microservices-demo/blob/main/src/loadgenerator/locustfile.py
+
 import random
 from locust import HttpUser, TaskSet, between
 
 
 def index(l):
-    l.client.get("/productpage")
+    l.client.get("productpage")
+
+def login(l):
+    l.client.post("login", {
+        'username': 'username',
+        'passwd': 'password'})
 
 class UserBehavior(TaskSet):
     def on_start(self):
         index(self)
 
-    tasks = {index: 10}
+    tasks = {index: 10,
+            login: 2}
 
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
