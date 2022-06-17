33b1bd9feddbb353d911dfaa69a8cab15806143e
Michael Mente <mentemichael@live.com>
2022-06-17 13:05:21 +0200

    update

--- teastore/loadgen/teastore_browse.lua
+++ teastore/loadgen/teastore_browse.lua
@@ -7,7 +7,7 @@
 --[[
 	Global Variables. Initialized at load driver startup.
 --]]
-prefix = "http://10.1.1.1:8080/tools.descartes.teastore.webui/"
+prefix = "http://192.168.59.106:30080/tools.descartes.teastore.webui/"
 productviewcount = 30
 postIndex = {3, 11}
 
